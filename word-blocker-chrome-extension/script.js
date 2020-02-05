0bf69540f701f494e1aae5ab3d0033a057405ab1
Kaan Yilmaz <dyilmaz@ogr.iu.edu.tr>
2020-02-05 22:05:03 +0300

    fix regex

--- word-blocker-chrome-extension/script.js
+++ word-blocker-chrome-extension/script.js
@@ -1,4 +1,3 @@
-const word = "word";
 replaceText(document.body);
 
 function replaceText(element) {
@@ -6,13 +5,13 @@ function replaceText(element) {
     element.childNodes.forEach(replaceText)
   }
   else if (element.nodeType === Text.TEXT_NODE) {
-    if(element.textContent.match(`/${word}/gi`)) {
+    if(element.textContent.match(/word/gi)) {
       // element.parentElement.style.color = 'black';
       // element.parentElement.style.backgroundColor = 'black';
       const newElement = document.createElement('span');
-      newElement.innerHTML = element.textContent.replace(`/${word}/gi`, '<span class="text-attention">$1</span>')
+      newElement.innerHTML = element.textContent.replace(/(word)/gi, '<span class="text-attention">$1</span>')
       element.replaceWith(newElement)
     }
-    // element.textContent =  element.textContent.replace(/lorem/gi, 'yarasa');
+    // element.textContent =  element.textContent.replace(/word/gi, 'yarasa');
   }
 }
