d013eec2d4b38a13f189c1d39b0d8ea55c92db98
Kaan Yilmaz <douscriptist@gmail.com>
2020-05-03 23:40:58 +0300

    simple validation ~

---
+++ form-validator/script.js
@@ -0,0 +1,62 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const repassword = document.getElementById('repassword');
+
+// Event Listeners
+form.addEventListener('submit', function (e) {
+	e.preventDefault();
+
+	checkRequiredFields([username, email, password, repassword]);
+	checkLengthFields(username, 3, 15);
+	checkLengthFields(password, 6, 12);
+	isValidEmail(email);
+	checkPasswordsMathc(password, repassword);
+});
+
+// Display Success Border
+function showSuccess(element) {
+	const formControl = element.parentElement;
+	formControl.className = 'form-control success';
+}
+
+// Display Error Message
+function showError(element, message) {
+	const formControl = element.parentElement;
+	const smallElement = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	smallElement.innerText = message;
+}
+
+// Check if email is valid
+function isValidEmail(element) {
+	const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	regexEmail.test(String(element.value.trim()).toLowerCase())
+		? showSuccess(email)
+		: showError(email, 'Enter a valid email!');
+}
+
+// Check if fields empty
+function checkRequiredFields(elementsArr) {
+	elementsArr.forEach((element) => {
+		element.value.trim() === ''
+			? showError(element, `${element.name} is required.`)
+			: showSuccess(element);
+	});
+}
+
+// Check fields length
+function checkLengthFields(element, min, max) {
+	element.value.length < min || element.value.length > max
+		? showError(
+				element,
+				`${element.name} have to be between ${min} - ${max} characterss;`
+		  )
+		: showSuccess(element);
+}
+
+// Check match passwords
+function checkPasswordsMathc(pass1, pass2) {
+	pass1.value !== pass2.value && showError(pass2, 'Passwords do not match!');
+}
